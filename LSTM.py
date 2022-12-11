39f8f1f51f38e464d05bba66a777b0d3e618b73b
GloriaYinn <98197654+GloriaYinn@users.noreply.github.com>
2022-12-10 20:08:51 -0500

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -1,3 +1,5 @@
+# Acquisition of Stock Data
+
 import math
 import yfinance as yf
 import numpy as np
@@ -12,12 +14,15 @@ stock_data = yf.download('AAPL', start='2016-01-01', end='2021-10-01')
 stock_data.head()
 
 
+# Visualizing Stock Prices History
 plt.figure(figsize=(15, 8))
 plt.title('Stock Prices History')
 plt.plot(stock_data['Close'])
 plt.xlabel('Date')
 plt.ylabel('Prices ($)')
 
+
+# Preparation of training set
 close_prices = stock_data['Close']
 values = close_prices.values
 training_data_len = math.ceil(len(values)* 0.8)
@@ -39,6 +44,7 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 
+#Preparation of test set
 test_data = scaled_data[training_data_len-60: , : ]
 x_test = []
 y_test = values[training_data_len:]
@@ -49,6 +55,8 @@ for i in range(60, len(test_data)):
 x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
+
+# Setting Up LSTM Network Architecture
 model = keras.Sequential()
 model.add(layers.LSTM(100, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(layers.LSTM(100, return_sequences=False))
@@ -57,16 +65,18 @@ model.add(layers.Dense(1))
 model.summary()
 
 
+# Training LSTM Model
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, batch_size= 1, epochs=3)
 
 
+# Model Evaluation
 predictions = model.predict(x_test)
 predictions = scaler.inverse_transform(predictions)
 rmse = np.sqrt(np.mean(predictions - y_test)**2)
 rmse
 
-
+# Visualizing the Predicted Prices
 data = stock_data.filter(['Close'])
 train = data[:training_data_len]
 validation = data[training_data_len:]
